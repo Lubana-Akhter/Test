241c9264486d24355b8a7508f37edd2fc0e75d70
Lubana-Akhter <lubana3693@gmail.com>
2023-02-28 11:04:17 +0600

    test commit

---
+++ dynamicFunc.js
@@ -0,0 +1,5 @@
+const Name = function(value){
+    return value;
+}
+
+console.log(Name("Lubna"))
---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function Calculation(...numbers){
+  let sum = 0;
+   for(let i of numbers){
+    sum +=i;
+
+   }
+   console.log(sum);
+}
+Calculation(1,1,2,2);
